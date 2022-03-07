83885aec26326ffca1e3a4b96ca0181323317f61
Wciv <sanchezeguiajon@gmail.com>
2022-03-07 22:33:13 +0100

    main parcialmente funcional

--- Programacion/actividad03/main/Ejercicio02.java
+++ Programacion/actividad03/main/Ejercicio02.java
@@ -1,12 +1,21 @@
 package actividad03.main;
 
-public class Ejercicio02 {
-	int opElegida= 100;
-	do {
-		do {
-			
-		}while()
-		
-	}while(opElegida != 0);
+import java.io.IOException;
 
+import actividad03.introduceDatos.pregunta;
+import introduceDatos.*;
+
+public class Ejercicio02 {
+	public static void main(String[] args) throws IOException {
+		int opElegida = 100;
+		do {
+			do {
+				System.out.println("Oye amigo qué quieres hacer? 1-4");
+				//pregunta.pideEntero();
+				opElegida = pregunta.pideEntero();
+				System.out.print(opElegida);
+			}while(opElegida < 0 || opElegida > 4);
+			System.out.println("XD");
+		}while(opElegida != 0);
+	}
 }
