d9243cf94eec4883e1a7beadf7ba12084961488f
Wciv <sanchezeguiajon@gmail.com>
2022-03-07 22:27:02 +0100

    Finalizado la funcion introducir datos

--- Programacion/actividad03/introduceDatos/pregunta.java
+++ Programacion/actividad03/introduceDatos/pregunta.java
@@ -2,10 +2,17 @@ package actividad03.introduceDatos;
 import java.util.Scanner;
 public class pregunta {
 	
-	public static int pideEntero(String[] pregunta) {
+	public static int pideEntero() {
 		Scanner sc=new Scanner(System.in);
 		int n = sc.nextInt();
+		sc.close();
 		return (n);
 	}
-
+	
+	public static double pideDouble() {
+		Scanner sc=new Scanner(System.in);
+		int m = sc.nextDouble();
+		sc.close();
+		return (m);
+	}
 }
