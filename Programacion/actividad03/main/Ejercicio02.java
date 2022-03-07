db7956f1fc48c557a014f003c8a781362ffcef09
Wciv <sanchezeguiajon@gmail.com>
2022-03-07 22:54:52 +0100

    Funcion LinkiaCoins funcional

--- Programacion/actividad03/main/Ejercicio02.java
+++ Programacion/actividad03/main/Ejercicio02.java
@@ -3,6 +3,7 @@ package actividad03.main;
 import java.io.IOException;
 
 import actividad03.introduceDatos.pregunta;
+import actividad03.operaciones.OperacionesLinkia;
 import introduceDatos.*;
 
 public class Ejercicio02 {
@@ -13,9 +14,12 @@ public class Ejercicio02 {
 				System.out.println("Oye amigo qué quieres hacer? 1-4");
 				//pregunta.pideEntero();
 				opElegida = pregunta.pideEntero();
-				System.out.print(opElegida);
+				//System.out.print(opElegida); //debug
 			}while(opElegida < 0 || opElegida > 4);
-			System.out.println("XD");
+			if(opElegida == 1) {
+				OperacionesLinkia.convertirLinkiaCoins();
+			}
+			//System.out.println("XD"); //debug
 		}while(opElegida != 0);
 	}
 }
