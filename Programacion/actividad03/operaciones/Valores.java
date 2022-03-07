af13a19dc7108c2ef43ab935e5e08c58ec2d0417
Wciv <sanchezeguiajon@gmail.com>
2022-03-07 23:31:29 +0100

    Todas las funciones completadas

--- Programacion/actividad03/operaciones/Valores.java
+++ Programacion/actividad03/operaciones/Valores.java
@@ -1,5 +1,29 @@
 package actividad03.operaciones;
 
-public class Valores {
+import Java.lang.math;
+import actividad03.introduceDatos.pregunta;
 
+public class Valores {
+	public static void muestraPi() {
+		double numPi = Math.PI;
+		System.out.println("Valores de Pi: "+numPi);
+	}
+	
+	public static void muestraValorAbsoluto() {
+		double a = 0;
+		double b = 0;
+		System.out.println("Introduce un numero para calcular valor absoluto: ");
+		a = pregunta.pideDouble();
+		b = Math.abs(a);
+		System.out.println("El valor absoluto de "+a+" es: "+b);
+	}
+	
+	public static void muestraValorAleatorio() {
+		int a = 0;
+		double b = 0;
+		System.out.println("Introduce un numero para sacar un aleatorio: ");
+		a = pregunta.pideEntero();
+		b = Math.random() * a;
+		System.out.println("Valor aleatorio entre 0 y "+a+": "+b);
+	}
 }
