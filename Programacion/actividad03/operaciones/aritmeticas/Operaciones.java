aabd0d22ed0faef4e9949f66d3ee5e64c5c61d89
Wciv <sanchezeguiajon@gmail.com>
2022-03-08 20:49:08 +0100

    Calcular ln

--- Programacion/actividad03/operaciones/aritmeticas/Operaciones.java
+++ Programacion/actividad03/operaciones/aritmeticas/Operaciones.java
@@ -1,5 +1,13 @@
 package actividad03.operaciones.aritmeticas;
 
+import Java.lang.math;
+
 public class Operaciones {
+	
+	public static double muestraLogaritmo(double logVal) {
+		double a = 0;
+		a = Math.log(logVal);
+		return a;
+	}
 
 }
