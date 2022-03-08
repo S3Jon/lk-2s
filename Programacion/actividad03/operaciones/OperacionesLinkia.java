4760a2baf3268a6b71664dfecc9565c23e1039e2
Wciv <sanchezeguiajon@gmail.com>
2022-03-08 20:51:48 +0100

    Pasar balance a LinkiaCoins. +Clear code

--- Programacion/actividad03/operaciones/OperacionesLinkia.java
+++ Programacion/actividad03/operaciones/OperacionesLinkia.java
@@ -5,7 +5,8 @@ import actividad03.introduceDatos.pregunta;
 public class OperacionesLinkia {
 	public static void convertirLinkiaCoins() {
 		System.out.print("Cuantos leuros quieres meter?: ");
-		double n = 0; double m = 0;
+		double n = 0; 
+		double m = 0;
 		n = pregunta.pideDouble();
 		m = n * 1.5;
 		System.out.println(n+" leuros equivalen a "+m+" LinkiaCoins");
