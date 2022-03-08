16bcc67912115b4941e090df4179a911ef7da77c
Wciv <sanchezeguiajon@gmail.com>
2022-03-08 21:05:46 +0100

    Operaciones aritmeticas terminado

--- Programacion/actividad03/operaciones/aritmeticas/Operaciones.java
+++ Programacion/actividad03/operaciones/aritmeticas/Operaciones.java
@@ -7,7 +7,15 @@ public class Operaciones {
 	public static double muestraLogaritmo(double logVal) {
 		double a = 0;
 		a = Math.log(logVal);
-		return a;
+		System.out.println("El logaritmo neperiano de "+logVal+" es: "+a);
+		return 0;
+	}
+	
+	public static double calculaPotencia(double Val1, double Val2) {
+		double a = 0;
+		a = Math.pow(Val1, Val2);
+		System.out.println(Val1+" elevado a "+Val2+" es igual a: "+a);
+		return 0;
 	}
 
 }
