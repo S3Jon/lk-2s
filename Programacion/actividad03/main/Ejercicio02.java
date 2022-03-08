f3859461ca4403ba9302cc54f45fd06282621384
Wciv <sanchezeguiajon@gmail.com>
2022-03-08 20:48:14 +0100

    Menu v2

--- Programacion/actividad03/main/Ejercicio02.java
+++ Programacion/actividad03/main/Ejercicio02.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import actividad03.introduceDatos.pregunta;
 import actividad03.operaciones.OperacionesLinkia;
 import actividad03.operaciones.Valores;
+import actividad03.operaciones.aritmeticas.Operaciones;
 import introduceDatos.*;
 
 public class Ejercicio02 {
@@ -12,28 +13,36 @@ public class Ejercicio02 {
 		int opElegida = 100;
 		do {
 			do {
-				System.out.println("Oye amigo qué quieres hacer? 1-4");
-				//pregunta.pideEntero();
+				imprimirMenu();
 				opElegida = pregunta.pideEntero();
-				//System.out.print(opElegida); //debug
-				System.out.println("1- Pasar dinero a LinkiaCoins.");
-				System.out.println("2- Muestra valores de Pi.");
-				System.out.println("3- Muestra el valor absoluto de un valor.");
-				System.out.println("4- Muestra un valor aleatorio con el rango elegido.");
 			}while(opElegida < 0 || opElegida > 12);
 			if(opElegida == 1) {
 				OperacionesLinkia.convertirLinkiaCoins();
 			}
-			if(opElegida == 2) {
+			if(opElegida == 2) 
 				Valores.muestraPi();
-			}
-			if(opElegida == 3) {
+			if(opElegida == 3) 
 				Valores.muestraValorAbsoluto();
-			}
-			if(opElegida == 4) {
+			if(opElegida == 4) 
 				Valores.muestraValorAleatorio();
+			if(opElegida == 5) {
+				double a = 0;
+				double b = 0;
+				System.out.print("Introduce un valor para calcular el logaritmo en base e: ");
+				a = pregunta.pideDouble();
+				b = Operaciones.muestraLogaritmo(a);
+				System.out.println("El logaritmo en base e de "+a+" es: "+b);
 			}
-			//System.out.println("XD"); //debug
 		}while(opElegida != 0);
+	}
+	
+	public static void imprimirMenu() {
+		System.out.println("1- Pasar dinero a LinkiaCoins.");
+		System.out.println("2- Muestra valores de Pi.");
+		System.out.println("3- Muestra el valor absoluto de un valor.");
+		System.out.println("4- Muestra un valor aleatorio con el rango elegido.");
+		System.out.println("5- Calcular el logaritmo neperiano de un número");
+		System.out.println("0- Salir.");
+		System.out.print("¿Qué quieres hacer?: ");
 	}
 }
