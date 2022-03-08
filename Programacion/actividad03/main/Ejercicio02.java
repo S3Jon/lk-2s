34209c1b4ff192206a5e22c000863f2a5c91e336
Wciv <sanchezeguiajon@gmail.com>
2022-03-08 21:05:03 +0100

    Menu v2.1

--- Programacion/actividad03/main/Ejercicio02.java
+++ Programacion/actividad03/main/Ejercicio02.java
@@ -31,7 +31,15 @@ public class Ejercicio02 {
 				System.out.print("Introduce un valor para calcular el logaritmo en base e: ");
 				a = pregunta.pideDouble();
 				b = Operaciones.muestraLogaritmo(a);
-				System.out.println("El logaritmo en base e de "+a+" es: "+b);
+			}
+			if(opElegida == 6) {
+				double a = 0;
+				double b = 0;
+				System.out.print("Introduce el primer valor: ");
+				a = pregunta.pideDouble();
+				System.out.print("Introduce el segundo valor: ");
+				b = pregunta.pideDouble();
+				Operaciones.calculaPotencia(a, b);
 			}
 		}while(opElegida != 0);
 	}
@@ -42,6 +50,7 @@ public class Ejercicio02 {
 		System.out.println("3- Muestra el valor absoluto de un valor.");
 		System.out.println("4- Muestra un valor aleatorio con el rango elegido.");
 		System.out.println("5- Calcular el logaritmo neperiano de un número");
+		System.out.println("6- Calcular un valor elevado a otro.");
 		System.out.println("0- Salir.");
 		System.out.print("¿Qué quieres hacer?: ");
 	}
