730000bd64ca634cadaed0b61bd94536e403e8f7
Wciv <sanchezeguiajon@gmail.com>
2022-03-08 19:47:55 +0100

    Imprimo texto para cada opcion

--- Programacion/actividad03/main/Ejercicio02.java
+++ Programacion/actividad03/main/Ejercicio02.java
@@ -16,13 +16,21 @@ public class Ejercicio02 {
 				//pregunta.pideEntero();
 				opElegida = pregunta.pideEntero();
 				//System.out.print(opElegida); //debug
-			}while(opElegida < 0 || opElegida > 4);
+				System.out.println("1- Pasar dinero a LinkiaCoins.");
+				System.out.println("2- Muestra valores de Pi.");
+				System.out.println("3- Muestra el valor absoluto de un valor.");
+				System.out.println("4- Muestra un valor aleatorio con el rango elegido.");
+			}while(opElegida < 0 || opElegida > 12);
 			if(opElegida == 1) {
 				OperacionesLinkia.convertirLinkiaCoins();
 			}
 			if(opElegida == 2) {
 				Valores.muestraPi();
+			}
+			if(opElegida == 3) {
 				Valores.muestraValorAbsoluto();
+			}
+			if(opElegida == 4) {
 				Valores.muestraValorAleatorio();
 			}
 			//System.out.println("XD"); //debug
