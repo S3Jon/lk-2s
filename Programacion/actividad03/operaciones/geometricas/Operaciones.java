c7c0288056870beb6283a0f77fefabac8b76e776
Wciv <sanchezeguiajon@gmail.com>
2022-03-08 22:00:53 +0100

    Finalizado operaciones geometricas

--- Programacion/actividad03/operaciones/geometricas/Operaciones.java
+++ Programacion/actividad03/operaciones/geometricas/Operaciones.java
@@ -1,5 +1,22 @@
 package actividad03.operaciones.geometricas;
-
+import actividad03.introduceDatos.pregunta;
 public class Operaciones {
-
+	
+	public static void muestraSeno() {
+		System.out.print("Introduce un numero para calcular el seno: ");
+		double a = 0;
+		double b = 0;
+		a = pregunta.pideDouble();
+		b = Math.sin(a);
+		System.out.println("El seno de "+a+" es: "+b);
+	}
+	
+	public static void muestraCoseno() {
+		System.out.print("Introduce un numero para calcular el coseno: ");
+		double a = 0;
+		double b = 0;
+		a = pregunta.pideDouble();
+		b = Math.cos(a);
+		System.out.println("El seno de "+a+" es: "+b);
+	}
 }
