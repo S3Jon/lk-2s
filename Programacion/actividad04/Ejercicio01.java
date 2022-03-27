1e1b2c86a1519f42996f53bf2021c4429daf92ef
Wciv <sanchezeguiajon@gmail.com>
2022-03-27 19:37:45 +0200

    Funcion 1 y 2 (parcial)

--- Programacion/actividad04/Ejercicio01.java
+++ Programacion/actividad04/Ejercicio01.java
@@ -9,12 +9,18 @@ public class Ejercicio01 {
 		System.out.println("Working Directory = " + System.getProperty("user.dir")); //  testeando el user.dir
 		File archivos = new File("archivos"); // Nombre carpeta
 		archivos.mkdir();					  // crea la carpeta
+		String local = System.getProperty("user.dir");
+		String path = local + File.separator + "archivos";
+		System.out.println(path);
 		do {
 			do {
 				imprimirMenu();
 				opElegida = pideEntero();
 			}while(opElegida < 0 || opElegida > 6);
-			//opcionElegida(opElegida);
+			if(opElegida == 1)
+				createNewFile(path);
+			if(opElegida == 2)
+				listFiles(path);
 		}while(opElegida != 0);
 	}
 	
@@ -34,8 +40,31 @@ public class Ejercicio01 {
 		int n = sc.nextInt();
 		return (n);
 	}
+	
+	public static String pideNombre() {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Introduce un nombre para el archivo: ");
+		String name = sc.next();
+		return (name);
+	}
 
-	public static void createNewFile() {
+	public static void createNewFile(String path) throws IOException {
+		String nmbre;
+		nmbre = pideNombre();
+		File nombre = new File(path+File.separator+nmbre+".txt");
+		nombre.createNewFile();
 		
+	}
+	
+	public static void listFiles(String path) {
+		String[] pnames;
+		File f = new File(path);
+		pnames = f.list();
+		int n = 1;
+		for (String pname : pnames)
+		{
+			System.out.println(n+"-"+pname);
+			n++;
+		}
 	}
 }
