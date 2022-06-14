807915bb9997d332bf13db350f4f8d370142fe41
jon <sanchezeguiajon@gmail.com>
2022-06-14 03:31:39 +0200

    Clase artista de la que las otras heredarán

---
+++ Programacion/actividad05/Artista.java
@@ -0,0 +1,47 @@
+package actividad05;
+
+public class Artista {
+	public String DNI;
+	public String nombre;
+	private int anyo;
+	private char sex;
+	
+	Artista(String DNA, String nambre, int anyyo, char sexo){
+		this.DNI=DNA;
+		this.nombre=nambre;
+		this.anyo=anyyo;
+		this.sex=sexo;
+	}
+	
+	public Artista(String DNA, String nombre, int anyo) {
+		this.DNI=DNA;
+		this.nombre=nombre;
+		this.anyo=anyo;
+	}
+	
+	public Artista() {
+	
+	}
+	
+	@Override
+	public String toString() {
+		return "Artista [DNI=" + DNI + ", nombre=" + nombre + ", anyo=" + anyo + ", sex=" + sex + "]";
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setAnyo(int anyo) {
+		this.anyo = anyo;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+
+}
